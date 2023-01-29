e96dfeb8e038758035d3768c8c427bb30c76cfc1
sridhar-5 <kv.sridharsai@gmail.com>
2023-01-29 16:07:40 +0530

    friends app almost done

---
+++ friends-app/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root "home#index"
+  get 'home/index'
+  get "home/about"
+end
