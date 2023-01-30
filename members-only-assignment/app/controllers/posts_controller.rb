fb8a173c59ded8dc5e6858d79cd14212d862e6b7
sridhar-5 <kv.sridharsai@gmail.com>
2023-01-30 11:59:19 +0530

    friends app working build

--- members-only-assignment/app/controllers/posts_controller.rb
+++ members-only-assignment/app/controllers/posts_controller.rb
@@ -1,2 +1,44 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  public
+  def create
+    # code here
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: "The post was successfully created..!"
+    else
+      @posts = Post.all
+      flash[:alert] = @post.errors.count
+      render :index, alert: 'Post was not created..!'
+    end
+  end
+
+  def index
+    @posts = Post.all.order("created_at DESC ")
+    @post = Post.new
+    @users = User.all
+  end
+
+  def show
+    @posts = Post.all.order("created_at DESC")
+    @members = User.all
+  end
+
+  public
+  def new
+    # code here
+    @post = current_user.posts.build
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- members-only-assignment/app/models/post.rb
+++ members-only-assignment/app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :body, :title, presence: true
+  belongs_to :user, dependent: :destroy
 end
